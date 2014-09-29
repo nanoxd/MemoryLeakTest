921341200a98318f5cc1d880816bd45002a1f0e3
Fernando Paredes <nano@fdp.io>
2014-09-28 21:20:14 -0700

    Initial commit

---
+++ MemoryLeakTest/Person.swift
@@ -0,0 +1,30 @@
+//
+//  Person.swift
+//  MemoryLeakTest
+//
+//  Created by Fernando Paredes on 9/28/14.
+//  Copyright (c) 2014 Fernando Paredes. All rights reserved.
+//
+
+import Foundation
+
+class Person {
+  let name: String
+  private let actionClosure: (() -> ())!
+
+  init(name: String) {
+    self.name = name
+
+    actionClosure = {
+      println("I am \(self.name)")
+    }
+  }
+
+  func performAction() {
+    actionClosure()
+  }
+
+  deinit {
+    println("\(name) is being deinitialized")
+  }
+}
---
+++ MemoryLeakTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemoryLeakTest
+//
+//  Created by Fernando Paredes on 9/28/14.
+//  Copyright (c) 2014 Fernando Paredes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+  
+}
+
